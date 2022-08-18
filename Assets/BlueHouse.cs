5a7e469df0e8a7633993fc49391f17583d8222c8
jiazeee <e0564914@u.nus.edu>
2022-08-18 13:06:56 +0800

    Add Blue House animation

---
+++ Assets/BlueHouse.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlueHouse : MonoBehaviour
+{
+    private Sprite[] sprites;
+    private Sprite[] BlueHouse2;
+
+    void Awake()
+    {
+        sprites = Resources.LoadAll<Sprite>("Sprites/Shop/Shop Blue House");
+        BlueHouse2 = Resources.LoadAll<Sprite>("Sprites/Shop/BlueHouse2");
+    }
+
+    void OnMouseOver()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = BlueHouse2[0];
+    }
+
+    void OnMouseExit()
+    {
+        gameObject.GetComponent<SpriteRenderer>().sprite = sprites[0];
+    }
+}
